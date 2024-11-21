70b0ebbac7bd8a67d053263637060c778d02f7ee
Idora <b64enigmas@gmail.com>
2024-11-20 22:09:31 -0300

    Delete kLoom directory

--- kLoom/nkbot.py
+++
@@ -1,8 +0,0 @@
-def nk():
-    import pyautogui as bot 
-
-    bot.PAUSE = 0.5
-    bot.hotkey('win', 'r')
-    bot.write('idle3')
-    bot.press('enter')
-nk()
--- kLoom/rldkloom.py
+++
@@ -1,7 +0,0 @@
-import pyautogui as bot 
-import time
-
-bot.PAUSE = 0.5
-time.sleep(0.5)
-bot.write('python kloom.py')
-bot.press('enter')
